c20672edc5a72e5af670d124bb5309ba1f3c0d59
Luiz Afonso <luizcosta12322@yahoo.com>
2025-04-29 20:10:18 -0300

    Initial commit

---
+++ DisciplinesRecommendationSystem/src/main/java/com/luiz/DisciplinesRecommendationSystem/DisciplinesRecommendationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.luiz.DisciplinesRecommendationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisciplinesRecommendationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisciplinesRecommendationSystemApplication.class, args);
+	}
+
+}
---
+++ DisciplinesRecommendationSystem/src/test/java/com/luiz/DisciplinesRecommendationSystem/DisciplinesRecommendationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz.DisciplinesRecommendationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisciplinesRecommendationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
